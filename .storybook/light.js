4804ddbb5525fcbb887f95eb7f1e6cdb490d0e6c
Khiem Le <lethienkhiem1111@gmail.com>
2024-01-31 15:13:56 +0700

    Installation step has been completed

---
+++ .storybook/light.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Oxalis Storybook',
+  brandUrl: '',
+  brandImage: 'https://firebasestorage.googleapis.com/v0/b/blog-35a06.appspot.com/o/180109oxalis-logo%20(1)%201.png?alt=media&token=6a9de8b2-2ba8-4c25-a1f4-f394058e9899',
+  brandTarget: '_self',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import light from "./light";
+
+addons.setConfig({
+  theme: light,
+});
